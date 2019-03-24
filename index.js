01c6329045d93f204c237413320b9bcfa7b7079c
Quentin Goinaud <armaldio@gmail.com>
2019-03-24 17:03:59 -0400

    experimental electron version

--- index.js
+++ index.js
@@ -28,10 +28,26 @@ const prebuildVersion = async ({ runtime, abi }) => {
 const run = async () => {
   const supportedTargets  = nodeAbi.supportedTargets;
   const additionalTargets = nodeAbi.additionalTargets;
+  const unofficialTargets = [
+	  {runtime: 'electron', abi: '5.0.0-beta.6', lts: false}
+	];
 
   const everything = supportedTargets.concat(additionalTargets);
 
-  for (let i = 0; i < everything.length; i++) {
+  /* for (let i = 0; i < everything.length; i++) {
+    let version = everything[ i ];
+
+    try {
+      const ret = await prebuildVersion(version);
+      if (ret.error) console.error(ret.message);
+      else console.log(`${version.runtime}@v${version.abi}: ${ret.message}`);
+    } catch (e) {
+      console.log('There was an error building ' + version);
+      console.error(e);
+    }
+  } */
+  
+  for (let i = 0; i < unofficialTargets.length; i++) {
     let version = everything[ i ];
 
     try {
