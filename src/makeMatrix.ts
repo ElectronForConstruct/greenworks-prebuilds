fd3a1313895d9cc51969b82b5b6d63399bc108c5
Quentin Goinaud <armaldio@gmail.com>
2020-12-19 17:19:31 +0100

    -

--- src/makeMatrix.ts
+++ src/makeMatrix.ts
@@ -9,69 +9,21 @@ const Runtimes = ['nw.js', 'electron', 'node']
 const OSs = ['macos-latest', 'ubuntu-latest', 'windows-latest']
 
 const run = async (/* release: Release */): Promise<void> => {
-  /* let everything = await abis.getAll()
-
-  const electronTargets = getUnique(
-    everything.filter((entry) => entry.runtime === 'electron'),
-    'abi',
-  )
-  const nwjsTargets = getUnique(
-    everything.filter((entry) => entry && entry.runtime === 'nw.js'),
-    'abi',
-  )
-  const nodeTargets = getUnique(
-    everything.filter((entry) => entry.runtime === 'node'),
-    'abi',
-  )
-
-  everything = electronTargets.concat(nwjsTargets).concat(nodeTargets)
-
-  const json: any = {}
-  const matrix: any[] = []
-  for (let i = 0; i < everything.length; i += 1) {
-    const version = everything[i]
-
-    if (version.abi < 70) {
-      // eslint-disable-next-line
-      continue
-    }
-
-    try {
-      OSs.forEach((os) => {
-        Archs.forEach((arch) => {
-          // add unless 64 bit build on linux
-          if (
-            (version.runtime === 'electron' && os === 'macos-latest' && arch === 'ia32')
-          ) {
-            // console.log('pass')
-          } else {
-            matrix.push({
-              runtime: version.runtime,
-              abi: version.abi,
-              version: version.version,
-              arch,
-              os,
-            })
-          }
-        })
-      })
-    } catch (e) {
-      console.log('Unable to build for this version:', e.stdout)
-      console.log(e)
-    }
-  } */
-
   const json: any = {}
   const matrix: any[] = []
 
   OSs.forEach((os) => {
     Runtimes.forEach((runtime) => {
       Archs.forEach((arch) => {
-        matrix.push({
-          runtime,
-          arch,
-          os,
-        })
+        if (
+          !(os === 'macos-latest' && arch === 'ia32')
+        ) {
+          matrix.push({
+            runtime,
+            arch,
+            os,
+          })
+        }
       })
     })
   })
